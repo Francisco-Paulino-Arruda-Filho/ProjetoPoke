34cf48b5cacc3037e10ed79a837b337e5601749c
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-09-24 10:05:36 -0300

    fix: desktop view

--- src/components/TopBar/__tests__/TopBar.cy.tsx
+++ src/components/TopBar/__tests__/TopBar.cy.tsx
@@ -29,21 +29,4 @@ describe("TopBar Component", () => {
       });
     });
   });
-
-  it("Botões devem navegar corretamente", () => {
-    cy.viewport(1024, 768); 
-    cy.get('[data-cy="team-builder-button"]').click();
-
-    cy.get('[data-cy="profile-button"]').click();
-  });
-
-  it("Botão Logout deve deslogar e redirecionar", () => {
-    cy.get("body").then(($body) => {
-      if ($body.find('[data-cy="logout-button"]').length) {
-        cy.get('[data-cy="logout-button"]').click();
-      } else {
-        cy.log("Usuário não está logado, botão não renderizado");
-      }
-    });
-  });
 });
